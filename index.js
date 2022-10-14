7d5101791469ca3e90a610fc4af11130b9b81884
Kenan <mckenzie.kenan@yahoo.com>
2022-10-13 20:41:34 -0400

    Added manager prompt

--- index.js
+++ index.js
@@ -9,9 +9,67 @@ const inquirer = require('inquirer');
 const team = []
 //Manager prompt opens on start
 const addManager = () => {
-    return inquirer.prompt({
-        
-    })
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Manager name:', 
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter manager name");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Manager ID:",
+            validate: idInput => {
+                if  (isNaN(idInput)) {
+                    console.log ("Please enter manager's id")
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Manager email:",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ('Please enter valid email')
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Manager office number:",
+            validate: officeNumber => {
+                if  (isNaN(officeNumber)) {
+                    console.log ('Please enter valid office number')
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Would you like to add more team members?',
+            default: false
+        }
+    ])
 };
 //Asks to add new employees and redirects to engineer/intern 
 const newEmployee = () => {
