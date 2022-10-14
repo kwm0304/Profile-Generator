a568d0dc500fb4c5d45c6fa3f31b74286c4659f9
Kenan <mckenzie.kenan@yahoo.com>
2022-10-13 20:45:01 -0400

    Added employee prompt

--- index.js
+++ index.js
@@ -71,12 +71,89 @@ const addManager = () => {
         }
     ])
 };
-//Asks to add new employees and redirects to engineer/intern 
-const newEmployee = () => {
-    return inquirer.prompt({
 
-    })
+//Employee prompt w/ asking to add more team members at bottom
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: "New employee role:",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "Employee name:", 
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter an employee's name");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'number',
+            name: 'id',
+            message: "Employee ID:",
+            validate: nameInput => {
+                if  (isNaN(nameInput)) {
+                    console.log ("Please enter the employee's ID")
+                    return false; 
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Employee email:",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ('Please enter an email!')
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Employee GitHub:",
+            when: (input) => input.role === "Engineer",
+            validate: nameInput => {
+                if (nameInput ) {
+                    return true;
+                } else {
+                    console.log ("Please enter valid gitHub page")
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Employee school:",
+            when: (input) => input.role === "Intern",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter intern's school.")
+                }}
+        },
+        {
+            type: 'confirm',
+            name: 'addEmployee',
+            message: 'Would you like to add more team members?',
+            default: false
+        }
+    ])
 };
+
 //creates renderedoutput file and logs errors
 const writeFile = data => {
     fs.writeFile('./dist/renderedoutput.html', data, err => {
